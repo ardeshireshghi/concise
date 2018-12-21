437844f81645c91c22ef9bc9e459a75cf341bfcd
Ardeshir Eshghi <ardeshir.eshghi@fundingcircle.com>
2018-12-21 00:43:10 +0000

    Add sessionMiddleware as default middleware
    SessionMiddleware now wraps the matching routeHandler before
    invoking the call to handler

--- src/SuperServer/app.php
+++ src/SuperServer/app.php
@@ -6,6 +6,7 @@ use function SuperServer\Http\Request\url;
 use function SuperServer\Http\Request\path;
 use function SuperServer\Http\Request\parseRouteParamsFromPath;
 use function SuperServer\Http\Response\response;
+use function SuperServer\Http\Session\middleware as sessionMiddleware;
 use function SuperServer\FP\compose;
 use function SuperServer\FP\ifElse;
 use function SuperServer\RouteMatcher\createRouteMatcherFilter;
@@ -16,23 +17,27 @@ function createMatchRouteChecker() {
   };
 }
 
+function handlerWithDefaultMiddlewares($routeHandler) {
+  return sessionMiddleware([])($routeHandler);
+}
+
 function createRouteHandlerInvoker() {
   return function($matchingRoutes) {
     // Pick the first matching route
-    $selectedRoute = current($matchingRoutes);
-    $selectedRoute['handler'](parseRouteParamsFromPath($selectedRoute));
+    $handlerWithSessionMiddleware = handlerWithDefaultMiddlewares(current($matchingRoutes)['handler']);
+    return $handlerWithSessionMiddleware(parseRouteParamsFromPath(current($matchingRoutes)));
   };
 }
 
 function createRouteNotFoundHandler() {
   return function() {
-    response('Route for path: \"'.path().'\" not found');
+    return response('Route for path: \"'.path().'\" not found');
   };
 }
 
 function createRouteHandler() {
-  return function ($matchingRoutes) {
-    ifElse(
+  return function($matchingRoutes) {
+    return ifElse(
       createMatchRouteChecker(),
       createRouteHandlerInvoker(),
       createRouteNotFoundHandler()
