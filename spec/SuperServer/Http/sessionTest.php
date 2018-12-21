2e179ae2d1266634263822164dba0a86fb630a83
Ardeshir Eshghi <ardeshir.eshghi@fundingcircle.com>
2018-12-21 00:37:06 +0000

    Add session functions for session management
    Create 'sessionMiddleware' which wraps the route handler, takes session config
    and sets PHP session

---
+++ spec/SuperServer/Http/sessionTest.php
@@ -0,0 +1,49 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use function SuperServer\Http\Session\middleware as sessionMiddleware;
+use function SuperServer\Http\Session\set as setSession;
+
+class SessionTest extends TestCase
+{
+  public function testSessionMiddleware()
+  {
+    $routeHandler = function($params) {
+      return array_merge($params, [ 'routehandler_sideeffect' => 'someresponse' ]);
+    };
+
+    $expectedRouteResponse = [
+      'id' => 'somevalue',
+      'routehandler_sideeffect' => 'someresponse'
+    ];
+
+    $middlewareConfig = [
+      'session_name'  => 'super_server_fp_',
+      'lifetime'      => 3600,
+      'save_path'     => '/tmp'
+    ];
+
+    $handlerWithSessionMiddleware = sessionMiddleware($middlewareConfig)($routeHandler);
+
+    $this->assertTrue(is_callable($handlerWithSessionMiddleware));
+    $this->assertEquals($expectedRouteResponse, $handlerWithSessionMiddleware(['id' => 'somevalue']));
+  }
+
+  public function testSetSession()
+  {
+    $_SESSION = [
+      'name' => 'somename'
+    ];
+
+    $newSession = setSession([
+      'auth' => 'sometoken'
+    ]);
+
+    $this->assertEquals([
+      'name'  => 'somename',
+      'auth' => 'sometoken'
+    ], $newSession);
+  }
+}
+
+?>
---
+++ src/SuperServer/Http/session.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace SuperServer\Http\Session;
+
+use function SuperServer\Middleware\Factory\create as createMiddleware;
+use function SuperServer\FP\ifElse;
+
+const DEFAULT_SESSION_NAME = 'super_server_fp_';
+const DEFAULT_SESSION_LIFETIME_SECONDS = 3600;
+
+function defaultConfig() {
+  return [
+    'lifetime'     => DEFAULT_SESSION_LIFETIME_SECONDS,
+    'session_name'  => DEFAULT_SESSION_NAME
+  ];
+}
+
+function initialise($config) {
+  $config = (count($config) === 0) ? defaultConfig() : $config;
+  if (!headers_sent()) {
+    ini_set('session.name', $config['session_name']);
+    ini_set('session.gc_maxlifetime', $config['lifetime']);
+    session_set_cookie_params($config['lifetime']);
+
+    if (isset($config['save_path']) && file_exists($config['save_path'])) {
+      session_save_path(realpath($config['save_path']));
+    }
+    session_start();
+  }
+}
+
+function middleware($middlewareConfig) {
+  return createMiddleware(function(array $config = array(), $params) {
+    initialise($config);
+    return $params;
+  })($middlewareConfig);
+}
+
+function set(array $sessions = array()) {
+  return ifElse(function($sessions) {
+    return count($sessions) > 0;
+  })(function($sessions) {
+    array_walk($sessions, function($sessionValue, $sessionKey) {
+      $_SESSION[$sessionKey] = $sessionValue;
+    });
+    return $_SESSION;
+  })(function() {
+    return $_SESSION;
+  })($sessions);
+}
